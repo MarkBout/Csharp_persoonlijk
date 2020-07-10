0ee0ab7ae6aef83e79043067e434606c9bb3ef2b
MarkBout <54793158+MarkBout@users.noreply.github.com>
2020-07-10 15:10:34 +0200

    Extra

---
+++ SQL_input/SqlBusiness.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SQL_input
+{
+
+    public class SqlBusiness
+    {
+        Variables vars = new Variables();
+       public static MySqlConnection DBconnection;
+        public static string MysqlConnentionString;
+        //Dynamisch verbinding maken met DB
+        public object DBconnect(string ipadd, string port, string username, string password, string dbname)
+        {
+            MysqlConnentionString = "datasource=" + ipadd + ";port=" + port + ";username=" + username + ";password=" + password + ";database=" + dbname;
+
+            try
+            {
+                DBconnection = new MySqlConnection(MysqlConnentionString);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e + "\n" + "Null error bij MySQLConnectionString.", "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+            Console.WriteLine(MysqlConnentionString);
+            return MysqlConnentionString;
+        }
+        public bool checkConn = false;
+        //kijken of de connectie van de DB open of dicht is
+        public bool isConected()
+        {
+            if (DBconnection.State == ConnectionState.Closed)
+            {
+                DBconnection.Open();
+                MessageBox.Show("Connection open");
+                checkConn = true;
+                return true;
+            }
+            else if (DBconnection.State == ConnectionState.Open)
+            {
+                checkConn = true;
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ SQL_input/Variables.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_input
+
+{
+   public class Variables
+    {
+        //variabelen voor de main window
+        private static string sqlCommand = "";
+        private static DataTable dataTable = new DataTable();
+
+
+        //variabelen SQL connectie
+        private static string ipaddress = "";
+        private static string port = "";
+        private static string username = "";
+        private static string password = "";
+        private static string database = "";
+
+        //getters & setters
+
+        public DataTable DataTable
+        {
+           get { return dataTable; }
+           set { dataTable = value; }
+        }
+        public string Sqlcommand
+        {
+            get { return sqlCommand; }
+            set { sqlCommand = value; }
+        }
+
+        //connectie getters & setters
+
+        public string Ipadress
+        {
+            get { return ipaddress; }
+            set { ipaddress = value; }
+        }
+
+        public string Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Database
+        {
+            get { return database; }
+            set { database = value; }
+        }
+    }
+}
